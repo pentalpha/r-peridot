8a2d435f09f3a5664be00695205bd741bb1be9fc
Pitágoras Alves <alves.pitagoras@gmail.com>
2019-09-24 01:51:10 -0300

    The PeridotConfig is now stored as a JSON file, using the org.json library.

--- src/main/java/peridot/Archiver/PeridotConfig.java
+++ src/main/java/peridot/Archiver/PeridotConfig.java
@@ -1,17 +1,16 @@
 package peridot.Archiver;
 
+import org.json.JSONArray;
 import peridot.Log;
 import peridot.script.r.Interpreter;
 import peridot.script.r.VersionNumber;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import org.json.JSONObject;
+
+import java.util.*;
 
 /**
  * Changing, saving and loading configurations like preferred R environment and packages repository
@@ -24,9 +23,19 @@ public class PeridotConfig implements Serializable{
     public String lastInputDir;
     public String rPeridotWebSite = "http://www.bioinformatics-brazil.org/r-peridot/";
     public static VersionNumber preferredRVersion = new VersionNumber("3.4");
-    public static VersionNumber minimalRVersion = new VersionNumber("3.4");
+    public static VersionNumber minimalRVersion = new VersionNumber("3.3");
 
     public PeridotConfig(){
+        setDefaults();
+    }
+
+    public PeridotConfig(boolean set_defaults){
+        if(set_defaults){
+            setDefaults();
+        }
+    }
+
+    public void setDefaults(){
         this.packagesRepository = rPeridotWebSite + "R/drat/";
         this.availableInterpreters = Places.getDefaultRexecs();
         this.defaultInterpreter = null;
@@ -55,10 +64,11 @@ public class PeridotConfig implements Serializable{
     private static PeridotConfig getConfigs(){
         PeridotConfig loadedConfig = null;
         if(Places.peridotConfigFile.exists()){
-            loadedConfig =
+            /*loadedConfig =
                     (PeridotConfig) Persistence.loadObjectFromBin(
                             Places.peridotConfigFile.getAbsolutePath()
-                    );
+                    );*/
+            loadedConfig = parseJsonConfig(Places.peridotConfigFile.getAbsolutePath());
         }
         if(loadedConfig == null){
             return new PeridotConfig();
@@ -78,6 +88,52 @@ public class PeridotConfig implements Serializable{
 
     public static void save() throws IOException{
         _instance.updateValues();
-        Persistence.saveObjectAsBin(Places.peridotConfigFile, _instance);
+        //Persistence.saveObjectAsBin(Places.peridotConfigFile, _instance);
+        saveJsonConfig(Places.peridotConfigFile.getAbsolutePath(), _instance);
+    }
+
+    public static PeridotConfig parseJsonConfig(String path){
+        File f = new File(path);
+        String content = Manager.fileToString(f).toString();
+        JSONObject json = new JSONObject(content);
+        PeridotConfig config = new PeridotConfig(false);
+        if (json.has("lastInputDir")){
+            config.lastInputDir = json.getString("lastInputDir");
+        }
+        config.defaultInterpreter = json.getString("defaultInterpreter");
+        config.packagesRepository = json.getString("packagesRepository");
+        config.rPeridotWebSite = json.getString("rPeridotWebSite");
+        config.preferredRVersion = new VersionNumber(json.getString("preferredRVersion"));
+        config.minimalRVersion= new VersionNumber(json.getString("minimalRVersion"));
+
+        Collection<Object> availableInterpretersList = json.getJSONArray("availableInterpreters").toList();
+        config.availableInterpreters = new HashSet<>();
+        for (Object inter : availableInterpretersList){
+            config.availableInterpreters.add(inter.toString());
+        }
+
+        return config;
+    }
+
+    public static void saveJsonConfig(String path, PeridotConfig instance){
+        JSONObject json = new JSONObject();
+        if(instance.lastInputDir != null) {
+            json.put("lastInputDir", instance.lastInputDir);
+        }
+        json.put("defaultInterpreter",instance.defaultInterpreter);
+        json.put("packagesRepository",instance.packagesRepository);
+        json.put("rPeridotWebSite",instance.rPeridotWebSite);
+
+        json.put("preferredRVersion",instance.preferredRVersion.toString());
+        json.put("minimalRVersion",instance.minimalRVersion.toString());
+
+        JSONArray array = new JSONArray();
+        for(String inter : instance.availableInterpreters){
+            array.put(inter);
+        }
+        json.put("availableInterpreters", array);
+
+        String toPrint = json.toString(4);
+        Manager.stringToFile(path, toPrint);
     }
 }
